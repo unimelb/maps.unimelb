27406966e4c13f8ae1cedd9f76efdabc9b374e23
Andrew Harris <aharris@unimelb.edu.au>
2019-03-14 11:36:33 +1100

    Rewritten by Jo So.
    Fixed the sorting problem.

--- maps [22896]/Maps (site) [28210]/Assets [28211]/inside.building.sorted.maze.api[76946].js
+++ maps [22896]/Maps (site) [28210]/Assets [28211]/inside.building.sorted.maze.api[76946].js
@@ -1,86 +1,31 @@
-var jsonResponse = JSON.parse(_REST.responses[0].body);
+var html = "<p id='poilist'>Sorry, no items found inside this building. <a href='#map'>Back to the map<\/a>.<\/p>";
 
-function parsePois(pois) { // organises the pois for output
-    parsedPois = [];
-	for(var currentPoiIndex = 0, totalPois = pois.length, currentPoi = ""; currentPoiIndex<totalPois; currentPoiIndex++) {
-		var currentPoi = pois[currentPoiIndex];
-	    var parsedPoi = [];
-	    var type = "";
-	    var typeMatch = /[LTS][BM]?[0-9]{1,4}/; // lift, toilet, stairwell
-	    if (typeMatch.test(currentPoi.title.substring(0, 5))){
-	    	type = currentPoi.title.charAt(0);
-	    }
-		parsedPoi.push({
-			z: currentPoi.z,
-			id: currentPoi.poiId,
-			label: currentPoi.title,
-			level: currentPoi.floorName,
-			type: type
-		});
-		parsedPois.push(parsedPoi);
-	}
-	function sortL(a, b) {
-	    /*
-	   if (a[0].label.toString() < b[0].label.toString()) return -1;
-	   if (a[0].label.toString() > b[0].label.toString()) return 1;
-	   return 0;
-	   */
-   	   return a[0].label.toString().localeCompare(b[0].label.toString());
-	}
-	function sortZ(a, b) {
-	   return a[0].z - b[0].z;
-	}
-	parsedPois = parsedPois.sort(sortL); // sort room names
-	parsedPois = parsedPois.sort(sortZ); // then sort floors by the more reliable zLevel
-	formatHTML(parsedPois); // all sorted
-}
+if (_REST.response.info.http_code == 200) {
+    var data = JSON.parse(_REST.response.body);
+    var rooms = {};
+    var floors = {};
 
-function formatHTML(parsedPois) { // prepares and prints the html output
-	if (parsedPois){
-		htmlHead = "<h2 class='heading-card' id='poilist'>Full list of rooms<\/h2>";
-		htmlBody = "";
-		htmlFoot = "<\/ul><\/accordion>";
-		cLevel = "";
-		typeClass = "";
+    var typeMatch = /[LTS][BM]?[0-9]{1,4}/; // lift, toilet, stairwell
 
-		for(var i = 0, t = parsedPois.length, c = ""; i<t; i++) {
-			c = parsedPois[i];
-			if (c[0].z != cLevel){
-				if (cLevel){
-					htmlBody += "<\/ul><\/accordion>";
-				}
-				htmlBody += "<accordion name='Level " + c[0].level + "'><ul>";
-			}
-			cLevel = c[0].z;
-			if ((c[0].type == "T") || (c[0].type == "L") || (c[0].type == "S")){
-				typeClass = "type-" + c[0].type;
-			}else{
-				typeClass = "";
-			}
-			/*
-			if ((c[0].label.substring(0, 1) == "T") || (c[0].label.substring(0, 1) == "L") || (c[0].label.substring(0, 1) == "S")){
-				typeClass = "type-" + c[0].label.substring(0, 1);
-			}else{
-				typeClass = "";
-			}
-			*/
-			htmlBody += "<li class='" + typeClass + "'><a href='https://maps.unimelb.edu.au/point?poi=" + c[0].id + "' rel='nofollow'>" + c[0].label + "<\/a><\/li>";
-		}
-	} else {
-		htmlHead = "<p id='poilist'>Sorry, no items found inside this building. <a href='#map'>Back to the map<\/a>.<\/p>";
-		htmlBody = "";
-		htmlFoot = "";
-	}
-	print(htmlHead + htmlBody + htmlFoot);
-}
+    if (data.pois.length) {
+        html = "<h2 class='heading-card' id='poilist'>Full list of rooms<\/h2>";
+        data.pois.forEach(function(poi){
+            if (!(poi.z in rooms))
+            {
+                rooms[poi.z] = {};
+                floors[poi.z]=poi.floorName;
+            }
+            rooms[poi.z][poi.title]= poi.poiId;
+           
+        });
+        Object.keys(floors).sort(function(a, b){return a-b}).forEach(function(level) {
+            html+="<accordion name='Level " + floors[level] +"'><ul>";
+            Object.keys(rooms[level]).sort().forEach(function(room) {
+                html+="<li " + (typeMatch.test(room.substring(0, 5))?"class='type-"+room.charAt(0)+"'":'') + "><a href='https://maps.unimelb.edu.au/point?poi=" + rooms[level][room] + "' rel='nofollow'>" + room + "<\/a><\/li>";
+            });
+            html+="<\/ul><\/accordion>";
+        });
+    } 
+} 
 
-if (jsonResponse){
-	var pois = jsonResponse.pois;
-	if (pois.length > 0){
-		parsePois(pois);
-	} else {
-   		formatHTML();
-	}
-} else {
-   	formatHTML();
-}
+print(html);
