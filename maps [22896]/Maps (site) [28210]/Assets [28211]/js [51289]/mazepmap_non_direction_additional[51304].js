10c164e3993bd43d14fc7ed4f8926da5d97ccf41
Andrew Harris <aharris@unimelb.edu.au>
2020-09-22 13:00:37 +1000

    Removing - temporarily, one hopes - the 'Get directions' links from pop-ups.

--- maps [22896]/Maps (site) [28210]/Assets [28211]/js [51289]/mazepmap_non_direction_additional[51304].js
+++ maps [22896]/Maps (site) [28210]/Assets [28211]/js [51289]/mazepmap_non_direction_additional[51304].js
@@ -46,20 +46,22 @@ var mapClick=function(lngLat, zLevel) {
 
 function popupPoiHTML(poi) {
     if (poi.properties.type == 'building')
-        	return '<h4>'+ (poi.properties.title!='null'?poi.properties.title:'')+'</h4><p>'+(poi.properties.buildingName?toTitleCase(poi.properties.buildingName):'')+(poi.properties.floorName?', Floor '
-	+toTitleCase(poi.properties.floorName):'')+'</p><p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?lngA='+poi.geometry.coordinates[0]+'&latA='+poi.geometry.coordinates[1]+'&zA=1" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?lngB='+poi.geometry.coordinates[0]+'&latB='+poi.geometry.coordinates[1]+'&zB=1" class="set-end">End here</a></p>';
+        	return '<h4>'+ (poi.properties.title!='null'?poi.properties.title:'')+'</h4><p>'+(poi.properties.buildingName?toTitleCase(poi.properties.buildingName):'')+(poi.properties.floorName?', Floor '+toTitleCase(poi.properties.floorName):'')+'</p>'
+        	/*+'<p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?lngA='+poi.geometry.coordinates[0]+'&latA='+poi.geometry.coordinates[1]+'&zA=1" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?lngB='+poi.geometry.coordinates[0]+'&latB='+poi.geometry.coordinates[1]+'&zB=1" class="set-end">End here</a></p>'*/
+        	;
     else
     	if(poi.properties.identifier==null){
-    	return '<h4>'+ (poi.properties.title!='null'?'<a href="%globals_asset_url:67217%?poi='+poi.properties.poiId+'">'+poi.properties.title+'</a>':'')+'</h4><p>'+(poi.properties.buildingName?toTitleCase(poi.properties.buildingName):'')+(poi.properties.floorName?', Floor '
-	+toTitleCase(poi.properties.floorName):'')+'</p><p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?pointA='+poi.properties.poiId+'" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?pointB='+poi.properties.poiId+'" class="set-end">End here</a></p>';
+    	return '<h4>'+ (poi.properties.title!='null'?'<a href="%globals_asset_url:67217%?poi='+poi.properties.poiId+'">'+poi.properties.title+'</a>':'')+'</h4><p>'+(poi.properties.buildingName?toTitleCase(poi.properties.buildingName):'')+(poi.properties.floorName?', Floor '+toTitleCase(poi.properties.floorName):'')+'</p>'
+		/*+'<p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?pointA='+poi.properties.poiId+'" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?pointB='+poi.properties.poiId+'" class="set-end">End here</a></p>'*/;
     	}else{
-    	return '<h4>'+ (poi.properties.title!='null'?'<a href="%globals_asset_url:67217%?identifier='+poi.properties.identifier+'">'+poi.properties.title+'</a>':'')+'</h4><p>'+(poi.properties.buildingName?toTitleCase(poi.properties.buildingName):'')+(poi.properties.floorName?', Floor '
-	+toTitleCase(poi.properties.floorName):'')+'</p><p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?pointA='+poi.properties.poiId+'" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?pointB='+poi.properties.poiId+'" class="set-end">End here</a></p>';
+    	return '<h4>'+ (poi.properties.title!='null'?'<a href="%globals_asset_url:67217%?identifier='+poi.properties.identifier+'">'+poi.properties.title+'</a>':'')+'</h4><p>'+(poi.properties.buildingName?toTitleCase(poi.properties.buildingName):'')+(poi.properties.floorName?', Floor '+toTitleCase(poi.properties.floorName):'')+'</p>'
+    	/*+'<p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?pointA='+poi.properties.poiId+'" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?pointB='+poi.properties.poiId+'" class="set-end">End here</a></p>'*/;
     	}
 }
 
 function popupLatLngHTML(lngLat, zLevel) {
-    return '<h4><a href="%globals_asset_url:67217%?lat='+lngLat.lat+'&lng='+lngLat.lng+'">Selected Location</a></h4><p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?lngA='+lngLat.lng+'&latA='+lngLat.lat+'&zA='+zLevel+'" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?lngB='+lngLat.lng+'&latB='+lngLat.lat+'&zB='+zLevel+'" class="set-end">End here</a></p>';
+    return '<h4><a href="%globals_asset_url:67217%?lat='+lngLat.lat+'&lng='+lngLat.lng+'">Selected Location</a></h4>'
+    /*+'<p><strong>Get directions:<strong></p><p><a href="%globals_asset_url:97098%?lngA='+lngLat.lng+'&latA='+lngLat.lat+'&zA='+zLevel+'" class="set-start">Start Here</a> or <a href="%globals_asset_url:97098%?lngB='+lngLat.lng+'&latB='+lngLat.lat+'&zB='+zLevel+'" class="set-end">End here</a></p>'*/;
 }
 
 function campusIdSwap(inVal){
